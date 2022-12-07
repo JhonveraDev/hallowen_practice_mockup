2d3427ee1b44522c597dc91e82ea15a684352f4e
Anderson Vera <70354869+JhonveraDev@users.noreply.github.com>
2022-12-07 11:28:36 -0500

    mockup_hallowen

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+/*=============== SHOW MENU ===============*/
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", openMenu);
+navClose.addEventListener("click", closeMenu);
+
+function openMenu() {
+  navMenu.classList.add("show-menu");
+}
+
+function closeMenu() {
+  navMenu.classList.remove("show-menu");
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const linkAction = () => {
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description,`);
+sr.reveal(`.home__tree-1`, { origin: "left", delay: 800 });
+sr.reveal(`.home__tree-2`, { origin: "right", delay: 800 });
+sr.reveal(`.home__img`, { delay: 800 });
+sr.reveal(`.category__card , .items__card`, { interval: 100 });
+sr.reveal(`.about__img, .about__data, .footer__tree-2`, { origin: "left" });
+sr.reveal(`.party__images, .party__data, .footer__tree-1`, { origin: "right" });
